b1f4f557f709f97496abeaf88952624777355b94
jose <joseignacio.fernandez@gmail.com>
2011-01-20 13:54:34 +0100

    Correction on slice selectors

--- lib/scrappy/selectors/slice.rb
+++ lib/scrappy/selectors/slice.rb
@@ -1,7 +1,7 @@
 module SliceSelector
   def self.filter selector, doc
     selector.rdf::value.map do |separator|
-      slices = doc[:content].text.split(separator)
+      slices = doc[:value].split(separator)
       selector.sc::index.map { |index| { :uri=>doc[:uri], :content=>doc[:content], :value=>slices[index.to_i].to_s.strip} }
     end.flatten
   end
