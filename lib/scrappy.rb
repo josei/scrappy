e9be34fb685054ec3fa884a16f79b9012782a8a3
jose <joseignacio.fernandez@gmail.com>
2011-02-25 09:36:51 +0100

    New gem with fixed dependency and more support for uri generation

--- lib/scrappy.rb
+++ lib/scrappy.rb
@@ -21,7 +21,7 @@ require 'scrappy/agent/agent'
 Namespace :sc, 'http://lab.gsi.dit.upm.es/scraping.rdf#'
 
 module Scrappy
-  VERSION = '0.1.19'
+  VERSION = '0.1.20'
 end
 
 # Require selectors
--- lib/scrappy/selectors/new_uri.rb
+++ lib/scrappy/selectors/new_uri.rb
@@ -7,8 +7,16 @@ module NewUriSelector
       [ doc[:content].text ]
     end
 
+    @@indexes ||= Hash.new(0)
+    prefix = selector.sc::prefix.first.to_s
+    prefix = (prefix =~ /\Ahttp/ ? URI::parse(doc[:uri]).merge(prefix).to_s : "#{doc[:uri]}#{prefix}")
+    suffix = selector.sc::suffix.first.to_s
+
     contents.map do |content|
-      new_uri = selector.sc::prefix.first.to_s + content.wikify + selector.sc::suffix.first.to_s
+      variable = selector.sc::sequence.first.to_s=="true" ? (@@indexes[selector] += 1) : content.wikify
+      
+      new_uri = "#{prefix}#{variable}#{suffix}"
+      
       { :uri=>new_uri, :content=>doc[:content], :value=>new_uri }
     end
   end
