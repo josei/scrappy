f749c97b3b39fe3319386d04e3994ce17c9e6db3
josei <joseignacio.fernandez@gmail.com>
2011-03-12 15:24:23 +0100

    Correction in function that adds visual data to a DOM tree

--- public/javascripts/scrappy.js
+++ public/javascripts/scrappy.js
@@ -3,17 +3,26 @@ add_visual_data = function() {
   var i=0;
   for(var i=0; i<items.length; i++) {
     var item = items[i];
-    item.setAttribute('vx', item.offsetLeft);
-    item.setAttribute('vy', item.offsetTop);
+    var x = 0;
+    var y = 0;
+    if (item.offsetParent) {
+      var obj = item;
+      do {
+			  x += obj.offsetLeft;
+			  y += obj.offsetTop;
+		  } while (obj = obj.offsetParent);
+	  }
+    item.setAttribute('vx', x);
+    item.setAttribute('vy', y);
     item.setAttribute('vw', item.offsetWidth);
     item.setAttribute('vh', item.offsetHeight);
-    item.setAttribute('vsize', document.defaultView.getComputedStyle(item, null).getPropertyValue('font-size'));
+    var size = document.defaultView.getComputedStyle(item, null).getPropertyValue('font-size');
+    size = size.substring(0, size.length-2);
+    item.setAttribute('vsize', size);
     var weight = document.defaultView.getComputedStyle(item, null).getPropertyValue('font-weight');
     if (weight == 'normal') weight = 400;
     if (weight == 'bold')   weight = 700;
     item.setAttribute('vweight', weight);
-    item.setAttribute('vcolor', document.defaultView.getComputedStyle(item, null).getPropertyValue('color'));
-    item.setAttribute('vbcolor', document.defaultView.getComputedStyle(item, null).getPropertyValue('background-color'));
   }
 }
 
