d998eb58598c012374d0926840cc573e14d5a5f9
jose <joseignacio.fernandez@gmail.com>
2011-03-15 10:57:47 +0100

    Minor fix to make kb editable in admin server

--- public/javascripts/annotator.js
+++ public/javascripts/annotator.js
@@ -64,42 +64,6 @@ jQuery(document).ready(function(){
   $("#scrappy_window").dialog();
 });
 
-/*
-$(document).ready(function(){
-  $("*").bind('click', function(e){
-    e.stopPropagation();
-    var element = $(e.target).closest(this.tagName).get(0).tagName;
-    var parents = $(this).parents();
-    var string = element.toString();
-    for(j=0;j<parents.length;j++) {
-      string = string + " " + parents[j].tagName;
-    }      
-    
-    var tree = [];
-    var treeString = "";
-    for(h=parents.length-1; h>=0; h-- ) {
-      tree.push(parents[h].tagName);
-      
-      if( treeString == "" ) {
-        treeString = treeString + parents[h].tagName;
-      } else {
-        treeString = treeString + " > " + parents[h].tagName;      
-      }
-    }                                 
-    
-    tree.push(element);
-    treeString = treeString + " > " + element;
-    
-    var myTrees = document.getElementById("myTrees");
-    var ul = document.createElement("ul");
-    var li = document.createElement("li");
-    myTrees.appendChild(ul);
-    li.innerHTML = treeString;
-    myTrees.appendChild(li);
-  });
-});
-*/
-
 add_visual_data();
 
 window.scrappy_loaded = true
