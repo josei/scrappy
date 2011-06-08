14d9423e6b22c3758e004483519e4c661cf3d3c1
jose <joseignacio.fernandez@gmail.com>
2011-06-08 15:52:03 +0200

    Bug correction

--- lib/scrappy/extractor/selectors/uri_pattern.rb
+++ lib/scrappy/extractor/selectors/uri_pattern.rb
@@ -2,7 +2,7 @@ module Sc
   class UriPatternSelector < Selector
     def filter doc
       # Check if the uri fits the pattern
-      if rdf::value.any? { |v| doc[:uri] =~ /\A#{v.gsub('.','\.').gsub('*', '.+')}\Z/ }
+      if rdf::value.any? { |v| doc[:uri] =~ /\A#{v.gsub('.','\.').gsub('*', '.+').gsub('?', '\?')}\Z/ }
         [ { :uri=>doc[:uri], :content=>doc[:content], :value=>format(doc[:value], sc::format, doc[:uri]) } ]
       else
         []
